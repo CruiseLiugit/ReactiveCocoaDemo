9e6b55e9d1720018447cb2c56ae81924c56c47f1
dajing <ddaajing@163.com>
2014-06-12 14:04:02 +0800

    remove unused code

--- ReactiveCocoaDemo/AddCityViewController.m
+++ ReactiveCocoaDemo/AddCityViewController.m
@@ -31,30 +31,31 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    // set right save button
+-(void)initUI {
     self.btnSave = [UIButton buttonWithType:UIButtonTypeContactAdd];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.btnSave];
-
+    
+    self.tfCityName.delegate = self;
+    
     // set slider vertical
     CGAffineTransform trans = CGAffineTransformMakeRotation(M_PI * 3 / 2);
     self.sliderPop.transform = trans;
+}
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self initUI];
+    
     [self bindViewModel];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
-{
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
     [textField resignFirstResponder];
     return YES;
 }
@@ -64,26 +65,19 @@
 
     // init viewModel
     self.viewModel = [AddCityViewModel new];
-    self.tfCityName.delegate = self;
+    
     // set validation once user input
     [[self.tfCityName.rac_textSignal distinctUntilChanged] subscribeNext:^(NSString *name) {
         @strongify(self);
-        
-        if([name isEqualToString:@"\n"]) {
-            [self.tfCityName resignFirstResponder];
-        }
-
         self.viewModel.cityName = name;
     }];
     
     RAC(self.btnSave, enabled) = self.viewModel.cityNameValidatorSignal;
     
-    [[self.btnSave rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
-        // set validation delay until button click
-
+    // set validation delay until button click
+//    [[self.btnSave rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
 //        self.viewModel.cityName = self.tfCityName.text;
-        [self.tfCityName resignFirstResponder];
-    }];
+//    }];
 
     // bind label textColor property to viewModel's cityNameValidatorSignal
     RAC(self.lblCityName, textColor) = [self.viewModel.cityNameValidatorSignal map:^id(NSNumber *valid) {
