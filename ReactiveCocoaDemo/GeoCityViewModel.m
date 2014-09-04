a610c5497d4a6a2a9e8364dc9da8f5c4b587d114
dajing <jingda@anjuke.com>
2014-09-04 17:08:00 +0800

    [updated] - 网络请求command的signal采用replaySignal避免多次side effects

--- ReactiveCocoaDemo/GeoCityViewModel.m
+++ ReactiveCocoaDemo/GeoCityViewModel.m
@@ -43,29 +43,62 @@ static NSString *const kSubscribeURL = @"http://api.geonames.org/citiesJSON?nort
     if (_searchSignal == nil) {
         // 第一种方式
         // use AFNetworking+Extensions
-//        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-//        manager.requestSerializer = [AFJSONRequestSerializer new];
-//        NSDictionary *params = @{@"user_id": self.uid};
-//         _searchSignal = [manager rac_GET:kSubscribeURL parameters:params];
-//        return _searchSignal;
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        manager.requestSerializer = [AFJSONRequestSerializer new];
+        NSDictionary *params = @{@"user_id": self.uid};
+         _searchSignal = [manager rac_GET:kSubscribeURL parameters:params];
+        return _searchSignal;
         
-        // 第二种方式
+        // 第二种方式,采用replay subject会只请求一次，以后的subscribe会直接取第一次的结果
         // create a signal with standard AFNetworking framework
-        return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-            AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-            [manager GET:kSubscribeURL parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                // send next signal once request is complete
-                [subscriber sendNext:responseObject];
-                // we should sendCompleted to release this searchSingal when signal is complete
-                [subscriber sendCompleted];
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                // sendError
-                [subscriber sendError:nil];
-            }];
+//        RACReplaySubject *subject = [RACReplaySubject subject];
+//
+//        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//        [manager GET:kSubscribeURL parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//            // send next signal once request is complete
+//            [subject sendNext:responseObject];
+//            // we should sendCompleted to release this searchSingal when signal is complete
+//            [subject sendCompleted];
+//        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//            // sendError
+//            [subject sendError:nil];
+//        }];
+//        
+//        return subject;
 
-            // no resoure to release, so return nil here
-            return nil;
-        }];
+        // 这种方式signal的side effect会在每次subscribe时被调用，
+//        return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+//            AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//            [manager GET:kSubscribeURL parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//                // send next signal once request is complete
+//                [subscriber sendNext:responseObject];
+//                // we should sendCompleted to release this searchSingal when signal is complete
+//                [subscriber sendCompleted];
+//            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//                // sendError
+//                [subscriber sendError:nil];
+//            }];
+//
+//            // no resoure to release, so return nil here
+//            return nil;
+//        }];
+        
+        // 第三种方式，用multiconnection来share signal的side effects
+//         RACSignal *searchSignal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+//            AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//            [manager GET:kSubscribeURL parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//                // send next signal once request is complete
+//                [subscriber sendNext:responseObject];
+//                // we should sendCompleted to release this searchSingal when signal is complete
+//                [subscriber sendCompleted];
+//            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//                // sendError
+//                [subscriber sendError:nil];
+//            }];
+//            
+//            return nil;
+//        }];
+//        return [[searchSignal publish] autoconnect];
         
     } // end if
     
